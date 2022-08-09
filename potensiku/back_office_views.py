a72d089d50a5600aa91d7b9741f4fa30d63ec9d6
fikri.nurwijaya <fikri.nurwijaya@wirecard.com>
2022-08-10 01:35:54 +0700

    update icon

--- potensiku/back_office_views.py
+++ potensiku/back_office_views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, HttpResponse, redirect
 from potensiku.questions import *
 from potensiku.models import *
+from potensiku.riasec_details import *
 import json
 from django.contrib.auth import authenticate, login, logout
 from types import SimpleNamespace
@@ -67,10 +68,8 @@ def index(request):
 @login_required(login_url='/backoffice/login')
 def participant_detail(request):
     token = request.POST.get("token")
-    # token = "axby"
     participant = Participant.objects.get(token=token)
     forms = build_detail_form()
-    databaru = ["data", "data", "data"]
     forms[0]["data"] = participant.person.full_name
     forms[1]["data"] = participant.person.nip
     forms[2]["data"] = participant.person.ttl
@@ -85,33 +84,22 @@ def participant_detail(request):
     formal_education.append("S2 - " + participant.person.educationS2)
     formal_education.append("S3 - " + participant.person.educationS3)
     forms[6]["data"] = formal_education
+
+    participant_result = calculate_answer(participant)
+    test_result_array, test_result_detail = calculate_result(participant_result)
+    test_result =""
+    for test_result_label in test_result_array:
+        test_result = test_result + test_result_label + ", "
+    test_result = test_result[:-2]
     # riwayat pengembangan
     self_improvement = [participant.person.history1, participant.person.history2, participant.person.history3]
     forms[7]["data"] = self_improvement
 
-    participant_result = ParticipantResult()
-    participant_result.token = participant.token
-    participant_result.name = participant.person.full_name
-    participant_result.nip = participant.person.nip
-    for answer in participant.answers.all():
-        if answer.answer == 1:
-            question = answer.question
-            if question.category == "R":
-                participant_result.r += 1
-            if question.category == "I":
-                participant_result.i += 1
-            if question.category == "A":
-                participant_result.a += 1
-            if question.category == "S":
-                participant_result.s += 1
-            if question.category == "E":
-                participant_result.e += 1
-            if question.category == "C":
-                participant_result.c += 1
-
     context = {
         "form_data": forms,
-        "result": participant_result
+        "result": participant_result,
+        "test_result": test_result,
+        "test_result_detail": test_result_detail
     }
     return render(request, 'potensiku_project/participant_detail.html', context)
 
@@ -134,6 +122,68 @@ def generate_token_screen(request):
     tokens = list(Token.objects.all().order_by("-created_date"))
     context["tokens"] = tokens
     return render(request, 'potensiku_project/generate_token.html', context)
+
+
+def calculate_answer(participant: Participant):
+    participant_result = ParticipantResult()
+    participant_result.token = participant.token
+    participant_result.name = participant.person.full_name
+    participant_result.nip = participant.person.nip
+    for answer in participant.answers.all():
+        if answer.answer == 1:
+            question = answer.question
+            if question.category == "R":
+                participant_result.r += 1
+            if question.category == "I":
+                participant_result.i += 1
+            if question.category == "A":
+                participant_result.a += 1
+            if question.category == "S":
+                participant_result.s += 1
+            if question.category == "E":
+                participant_result.e += 1
+            if question.category == "C":
+                participant_result.c += 1
+    return participant_result
+
+
+def calculate_result(participant_result: ParticipantResult):
+    test_result_detail = []
+    test_result = []
+    highest_result = 0
+    if highest_result < participant_result.r:
+        highest_result = participant_result.r
+    if highest_result < participant_result.i:
+        highest_result = participant_result.i
+    if highest_result < participant_result.a:
+        highest_result = participant_result.a
+    if highest_result < participant_result.s:
+        highest_result = participant_result.s
+    if highest_result < participant_result.e:
+        highest_result = participant_result.e
+    if highest_result < participant_result.c:
+        highest_result = participant_result.c
+
+    if participant_result.r == highest_result:
+        test_result_detail.append(get_riasec_for_r())
+        test_result.append("Realistic")
+    if participant_result.i == highest_result:
+        test_result_detail.append(get_riasec_for_i())
+        test_result.append("Investigative")
+    if participant_result.a == highest_result:
+        test_result_detail.append(get_riasec_for_a())
+        test_result.append("Artistic")
+    if participant_result.s == highest_result:
+        test_result_detail.append(get_riasec_for_s())
+        test_result.append("Social")
+    if participant_result.e == highest_result:
+        test_result_detail.append(get_riasec_for_e())
+        test_result.append("Enterprising")
+    if participant_result.c == highest_result:
+        test_result_detail.append(get_riasec_for_c())
+        test_result.append("Conventional")
+
+        return test_result, test_result_detail
 
 
 def logout_view(request):
---
+++ potensiku/riasec_details.py
@@ -0,0 +1,107 @@
+def get_riasec_for_r():
+    return {
+        "id": "R",
+        "name": "Realistic",
+        "explanation": "Menurut teori Holland, seseorang yang memiliki kepribadian "
+                       "realistis dianggap lebih suka bekerja secara manual dan menikmati "
+                       "pengoperasian perangkat (mesin) atau kendaraan. Banyak profesi pekerjaan "
+                       "yang realistis melibatkan aktivitas di luar ruangan, bekerja dengan hewan, "
+                       "dan mengharuskan seseorang untuk mengotori tangan mereka ketika bekerja. Profesi "
+                       "yang cocok untuk kepribadian realistis ini berbanding terbalik dengan pekerjaan "
+                       "kantor, di mana orang-orang cenderung duduk di dalam ruangan sepanjang hari. Terlebih lagi, "
+                       "menurut teori RIASEC, seseorang dengan kepribadian realistis cenderung memiliki hobi "
+                       "seperti berkebun, memelihara hewan peliharaan, mengotak-atik mobil, dan olahraga di luar ruangan."
+    }
+
+
+def get_riasec_for_i():
+    return {
+        "id": "I",
+        "name": "Investigative",
+        "explanation": "Individu memiliki preferensi terhadap "
+                       "aktivitas yang memerlukan penyelidikan "
+                       "observasional, simbolik, sistematis, dan "
+                       "kreatif terhadap fenomena fisik, biologis, "
+                       "dan budaya untuk memahami dan mengendalikan "
+                       "fenomena tersebut, dan tidak menyukai aktivitas "
+                       "persuasif, sosial, dan berulang. Contoh jenis "
+                       "ini adalah ahli kimia dan fisika. Individu "
+                       "dengan kepribadian ini lebih berorientasi pada "
+                       "konsep dan teori. Mereka bertindak lebih sebagai "
+                       "pemikir daripada sebagai pelaksana. Mereka sering "
+                       "menghindari hubungan interpersonal dan lebih memilih "
+                       "karir yang berhubungan dengan matematika dan sains."
+    }
+
+
+def get_riasec_for_a():
+    return {
+        "id": "A",
+        "name": "Artistic",
+        "explanation": "Individu lebih menyukai kegiatan-kegiatan yang ambigu, "
+                       "bebas, dan tidak sistematis untuk menciptakan produk "
+                       "seni seperti lukisan, drama, dan esai. Tidak menyukai "
+                       "kegiatan yang sistematis, teratur, dan rutin. Kompetensi "
+                       "dalam usaha artistik dikembangkan dan kemampuan rutin, "
+                       "sistematis, dan klerikal diabaikan. Lihat diri Anda sebagai "
+                       "orang yang ekspresif, murni, mandiri, dan mampu secara artistik. "
+                       "Ciri khasnya adalah emosional, imajinatif, impulsif, dan murni. "
+                       "Pekerjaan artistik seperti patung, akting, lukisan, dan komposisi. "
+                       "Individu dengan kepribadian ini lebih suka berinteraksi dengan "
+                       "dunianya melalui ekspresi artistik, cenderung menghindari situasi "
+                       "konvensional dan interpersonal. Individu seperti itu lebih "
+                       "berorientasi pada karir seperti seni dan menulis."
+    }
+
+
+def get_riasec_for_s():
+    return {
+        "id": "S",
+        "name": "Social",
+        "explanation": "Individu menyukai kegiatan yang berhubungan dengan orang lain seperti "
+                       "mengajar, membantu, dan memberikan bantuan. Tidak menyukai "
+                       "kegiatan rutin dan sistematis. Kompetensi sosial dikembangkan "
+                       "dan yang manual dan teknis diabaikan. Menganggap dirinya "
+                       "kompeten dalam mengajar dan membantu orang lain dan menikmati "
+                       "kegiatan sosial. Ciri khasnya adalah kooperatif, ramah, persuasif, "
+                       "dan bijaksana. Pekerjaan sosial meliputi pekerjaan seperti mengajar, "
+                       "konseling, dan pekerjaan sosial. Individu ini memiliki kemampuan verbal dan "
+                       "interpersonal yang baik. Mereka lebih cocok memasuki dunia karir yang berhubungan "
+                       "dengan manusia seperti mengajar, pekerjaan sosial, konseling, dan sejenisnya."
+    }
+
+
+def get_riasec_for_e():
+    return {
+        "id": "E",
+        "name": "Enterprising",
+        "explanation": "Individu lebih menyukai aktivitas yang melibatkan "
+                       "manipulasi orang lain. Tidak menyukai kegiatan yang "
+                       "sistematis, abstrak, dan ilmiah. Kompetensi kepemimpinan, "
+                       "persuasif, dan pengawasan dikembangkan dan kompetensi ilmiah "
+                       "diabaikan. Lihat diri Anda sebagai orang yang agresif, populer, "
+                       "percaya diri, dan memiliki keterampilan kepemimpinan. Ciri khasnya "
+                       "adalah ambisi, dominasi, optimisme, dan kemampuan bersosialisasi. "
+                       "Individu ini menggunakan kemampuan verbalnya untuk memimpin orang "
+                       "lain, mendominasi individu, dan menjual produk atau hal lainnya. "
+                       "Individu ini lebih cocok dalam karir seperti penjualan, politik, dan manajemen."
+    }
+
+
+def get_riasec_for_c():
+    return {
+        "id": "C",
+        "name": "Conventional",
+        "explanation": "Individu lebih menyukai kegiatan yang membutuhkan "
+                       "manipulasi data yang eksplisit, teratur, dan "
+                       "sistematis untuk berkontribusi pada tujuan organisasi. "
+                       "Menyukai kegiatan yang tidak bebas, sistematis, dan pasti. "
+                       "Kompetensi yang dikembangkan di bidang administrasi, komputasi, "
+                       "dan sistem bisnis dan kegiatan artistik diabaikan. Melihat diri "
+                       "sebagai terorganisir, mudah beradaptasi, memiliki kemampuan klerikal "
+                       "dan numerik. Ciri-cirinya adalah efisiensi, keteraturan, kepraktisan, "
+                       "dan pengendalian diri. Pekerjaan yang cocok seperti bankir, penilai, "
+                       "spesialis pajak, dan pemegang buku. Individu ini lebih menyukai kegiatan "
+                       "yang terstruktur. Mereka lebih menyukai pekerjaan berupa bawahan seperti "
+                       "pegawai bank, sekretaris, dan arsiparis."
+    }
